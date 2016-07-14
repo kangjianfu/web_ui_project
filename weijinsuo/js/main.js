a684a527de69e79370d87d5571650ce9587e2d3c
kangjf_lv_juan <551730672@qq.com>
2016-07-14 23:22:10 +0800

    提交

--- weijinsuo/js/main.js
+++ weijinsuo/js/main.js
@@ -5,7 +5,7 @@ $(function(){
     
  //
  $('[data-toggle="tooltip"]').tooltip();
-    
+    set_tab_width();
 })
 //设置轮播图的背景
 function set_carousel_back(){
@@ -21,4 +21,24 @@ function set_carousel_back(){
         }
         
     });
+}
+
+//控制标签页的标签宽度
+//
+function set_tab_width(){
+    var ul_container=$(".nav-tabs");
+
+    //获取其所有子元素的宽度的和
+    var tab_width=30;
+    //遍历子元素
+    console.info(ul_container.children().clientWidth)
+    ul_container.children().each(function(index, el) {
+        tab_width+=el.clientWidth
+    });
+
+    //判断当前宽度是否超出了屏幕宽度，如果超出了就显示横向滚动条
+    if(tab_width>$(window).width()){
+          $(ul_container).css('width',tab_width).parent().css('overflow-x','scroll');
+    }
+  
 }
